1bacc713fc02036816411fadd9cf73b780f507a2
Roberto Garza <robertg4567@hotmail.com>
2018-07-24 14:34:25 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,19 @@
 class Shoe
-  
+
   def initialize(brand)
     @brand = brand
   end
-  
+
   def brand
     @brand
   end
+
+  def color=(color)
+    @color = color
+  end 
+
+  def color 
+    @color 
+  end 
   
 end
